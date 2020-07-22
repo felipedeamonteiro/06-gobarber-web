68fa42a972057e80ddc0b13142d96cd81125215e
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-07-22 00:54:23 -0300

    Add validation to forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 import GlobalStyles from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignUp />
+    <SignIn />
     <GlobalStyles />
   </>
 );
